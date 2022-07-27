3e1cda34c350931c3a28a3d873a28d5dd6ee0195
Kevin Cavalcanti <k.w9cavalcanti@gmail.com>
2022-07-27 09:39:49 -0300

    feat: add config for module resolution and decorators

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,26 @@
 module.exports = {
-  presets: ["babel-preset-expo"],
+  presets: ["babel-preset-expo", 'module:metro-react-native-babel-preset'],
   env: {
     production: {},
   },
   plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extension: ['.js', '.ts', '.tsx'],
+        alias: {
+          '~': './app'
+        }
+      }
+    ],
+    // [
+    //   'babel-plugin-root-import',
+    //   {
+    //     rootPathPrefix: '~',
+    //     rootPathSuffix: 'app'
+    //   }
+    // ],
     [
       "@babel/plugin-proposal-decorators",
       {
@@ -12,4 +29,34 @@ module.exports = {
     ],
     ["@babel/plugin-proposal-optional-catch-binding"],
   ],
+  env: {
+    production: {
+      plugins: [
+        [
+          require.resolve('babel-plugin-module-resolver'),
+          {
+            cwd: 'babelrc',
+            extension: ['.js', '.ts', '.tsx', '.story', '.*'],
+            alias: {
+              '~': './app'
+            }
+          }
+        ],
+        // [
+        //   'babel-plugin-root-import',
+        //   {
+        //     rootPathPrefix: '~',
+        //     rootPathSuffix: 'app'
+        //   }
+        // ],
+        [
+          '@babel/plugin-proposal-decorators',
+          {
+            legacy: true,
+          }
+        ],
+        ['@babel/plugin-proposal-optional-catch-binding']
+      ]
+    },
+  }
 }
